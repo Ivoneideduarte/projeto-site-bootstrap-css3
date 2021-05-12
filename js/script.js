89b6ffa278fb79cb43850e62cf74d68bdf29ff54
Ivoneide Duarte <contato.ivoneideduarte@gmail.com>
2021-05-12 16:15:13 -0300

    Animação do scroll com JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,96 @@
-console.log('teste')
+$(document).ready(function () {
+
+    // Progress bar
+    let containerA = document.getElementById('circleA')
+    let circleA = new ProgressBar.Circle(containerA, {
+
+        color: '#64daf9',
+        strokeWidth: 8,
+        duration: 1400,
+        from: { color: '#aaa' },
+        to: { color: '#65daf9' },
+
+        step: function (state, circle) {
+            // Estrutura do circulo para animação
+            circle.path.setAttribute('stroke', state.color)
+
+            let value = Math.round(circle.value() * 60)
+
+            circle.setText(value)
+        }
+    })
+    // Progress bar
+    let containerB = document.getElementById('circleB')
+    let circleB = new ProgressBar.Circle(containerB, {
+
+        color: '#64daf9',
+        strokeWidth: 8,
+        duration: 1600,
+        from: { color: '#aaa' },
+        to: { color: '#65daf9' },
+
+        step: function (state, circle) {
+            // Estrutura do circulo para animação
+            circle.path.setAttribute('stroke', state.color)
+
+            let value = Math.round(circle.value() * 254)
+
+            circle.setText(value)
+        }
+    })
+    let containerC = document.getElementById('circleC')
+    let circleC = new ProgressBar.Circle(containerC, {
+
+        color: '#64daf9',
+        strokeWidth: 8,
+        duration: 1800,
+        from: { color: '#aaa' },
+        to: { color: '#65daf9' },
+
+        step: function (state, circle) {
+            // Estrutura do circulo para animação
+            circle.path.setAttribute('stroke', state.color)
+
+            let value = Math.round(circle.value() * 32)
+
+            circle.setText(value)
+        }
+    })
+    let containerD = document.getElementById('circleD')
+    let circleD = new ProgressBar.Circle(containerD, {
+
+        color: '#64daf9',
+        strokeWidth: 8,
+        duration: 2000,
+        from: { color: '#aaa' },
+        to: { color: '#65daf9' },
+
+        step: function (state, circle) {
+            // Estrutura do circulo para animação
+            circle.path.setAttribute('stroke', state.color)
+
+            let value = Math.round(circle.value() * 5423)
+
+            circle.setText(value)
+        }
+    })
+
+    // Iniciando o loader quanto o usuário chega no elemento
+    let dataAreaOffset = $('#data-area').offset()
+    let stop = 0
+
+    $(window).scroll(function (e) {
+        let scroll = $(window).scrollTop();
+
+        if (scroll > (dataAreaOffset.top - 500) && stop == 0) {
+
+            circleA.animate(1.0)
+            circleB.animate(1.0)
+            circleC.animate(1.0)
+            circleD.animate(1.0)
+
+            stop = 1
+        }
+    })
+
+})
