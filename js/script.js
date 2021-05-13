87cda3d45c36ac412a4753e1a4223a02cd99e916
Ivoneide Duarte <contato.ivoneideduarte@gmail.com>
2021-05-13 10:11:20 -0300

    Grid com parallax JS

--- js/script.js
+++ js/script.js
@@ -96,6 +96,7 @@ $(document).ready(function () {
     // Parallax
     setTimeout(function() {
         $('#data-area').parallax({imageSrc: 'img/cidadeparallax.png'})
+        $('#apply-area').parallax({imageSrc: 'img/pattern.png'})
     }, 250)
 
 })
